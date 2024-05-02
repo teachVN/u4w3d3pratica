c7470962ca76fa43833efe17214a6dc6ad986fde
teachVN <vinnap1976@gmail.com>
2024-05-02 18:11:33 +0200

    u4w3d3 pratica

--- src/main/java/epicode/ProvaEvento.java
+++ src/main/java/epicode/ProvaEvento.java
@@ -28,6 +28,7 @@ public class ProvaEvento {
         PersonaDao personaDao = new PersonaDao(em);
         PartecipazioneDao partecipazioneDao = new PartecipazioneDao(em);
 
+        //creo l'evento
         Evento e1 = new Evento();
         e1.setTipoEvento(TipoEvento.PUBBLICO);
         e1.setTitolo("Concerto 1 maggio");
@@ -35,45 +36,55 @@ public class ProvaEvento {
         e1.setDataEvento(LocalDate.of(2024,5,1));
         e1.setNumeroMassimoPartecipanti(50000);
 
+        //salvo l'evento sul db
         eventoDao.save(e1);
 
+        //creo la location
         Location location = new Location();
         location.setNome("Circo Massimo");
         location.setCitta("Roma");
-
+        //salvo la location sul db
         locationDao.save(location);
 
+        //aggancio la location all'evento
         e1.setLocation(location);
+        //risalvo l'evento
         eventoDao.save(e1);
 
+        //creo una persona
         Persona p1 = new Persona();
         p1.setNome("Dario");
         p1.setCognome("Sellitti");
         p1.setEmail("dfasljk");
         p1.setSesso(Sesso.M);
-
+        //salvo la persona
         personaDao.save(p1);
 
+        //creo un'altra persona
         Persona p2 = new Persona();
         p2.setNome("Mario");
         p2.setCognome("Stanco");
         p2.setEmail("dfasljk");
         p2.setSesso(Sesso.M);
-
+        //salvo la persona sul db
         personaDao.save(p2);
 
+        //creo una partecipazione passandogli la persona e l'evento
         Partecipazione partecipazione = new Partecipazione();
         partecipazione.setEvento(e1);
         partecipazione.setPersona(p1);
         partecipazione.setStato(Stato.DA_CONFERMARE);
 
+        //salvo la partecipazione
         partecipazioneDao.save(partecipazione);
 
+        //creo un'altra partecipazione passandogli la persona e l'evento
         Partecipazione partecipazione2 = new Partecipazione();
         partecipazione2.setEvento(e1);
         partecipazione2.setPersona(p2);
         partecipazione2.setStato(Stato.DA_CONFERMARE);
 
+        //salvo la seconda partecipazione
         partecipazioneDao.save(partecipazione2);
 
 
